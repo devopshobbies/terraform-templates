e1e454e3624961ffdfff98a517c2f9aec44037f9
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-03-23 18:42:38 +0000

    update fmt

--- part07-CiscoDevNet-provider/variables.tf
+++ part07-CiscoDevNet-provider/variables.tf
@@ -1,28 +1,28 @@
 variable "device_address" {
-  type = string
+  type        = string
   description = "Management IP Address"
 }
 variable "username" {
-  type = string
+  type        = string
   description = "SSH username"
 }
 variable "password" {
-  type = string
+  type        = string
   description = "SSH password"
 }
 variable "ntp_address" {
-  type = string
+  type        = string
   description = "NTP server IP Address"
 }
 variable "ntp_key" {
-  type = string
+  type        = string
   description = "NTP key, if required"
 }
 variable "local_subnet" {
-  type = string
+  type        = string
   description = "your local subnet need to be like this: 192.168.0.0/26"
 }
 variable "local_subnet_gw" {
-  type = string
+  type        = string
   description = "Gateway ip address to reach your local subnet"
 }
