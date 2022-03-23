e9fda0ef2a8d7f29b9f0545094993a3cc3210ffd
mozart4242 <sir.morteza4242@gmail.com>
2022-03-23 14:08:05 +0430

    Done

---
+++ part07-CiscoDevNet-provider/variables.tf
@@ -0,0 +1,21 @@
+variable "device_address" {
+  type = string
+}
+variable "username" {
+  type = string
+}
+variable "password" {
+  type = string
+}
+variable "ntp_address" {
+  type = string
+}
+variable "ntp_key" {
+  type = string
+}
+variable "local_subnet" {
+  type = string
+}
+variable "local_subnet_gw" {
+  type = string
+}
