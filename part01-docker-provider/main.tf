9c7bf4b6af2ee2c526390091d7b6469122bc222a
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-04-04 20:35:35 +0100

    update part01

--- part01-docker-provider/main.tf
+++ part01-docker-provider/main.tf
@@ -1,13 +1,3 @@
-# Adding terraform version and terraform provider version on Terraform block
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
---
+++ part01-docker-provider/providers.tf
@@ -0,0 +1,9 @@
+# Adding terraform version and terraform provider version on Terraform block
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
