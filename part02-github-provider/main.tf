79105e49bfde67dc64f785cee8b95a6df36c5cad
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2022-04-04 20:36:31 +0100

    update part02

--- part02-github-provider/main.tf
+++ part02-github-provider/main.tf
@@ -1,23 +1,3 @@
-# Adding terraform version and terraform provider version on Terraform block
-# GitHub Provider Docs: https://registry.terraform.io/providers/integrations/github/latest/docs
-
-terraform {
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "4.23.0"
-    }
-  }
-}
-
-
-provider "github" {
-  # for more authentication ways and other configure options go to https://registry.terraform.io/providers/integrations/github/latest/docs#authentication
-
-  token = "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"   # var.token
-}
-
-
 # Resources
 
 # This resource allows you to create and manage repositories within your GitHub organization or personal account.
