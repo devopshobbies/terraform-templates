f462c9d70bf859a0518cce15831749c549e4e839
Seyed Mahdi <63206853+seyedmahdi4@users.noreply.github.com>
2022-04-25 06:16:40 +0430

    Delete part15-openstack-provider directory

--- part15-openstack-provider/4-keypairs.tf
+++
@@ -1,4 +0,0 @@
-resource "openstack_compute_keypair_v2" "sshkey" {
-  name = "${var.prefix}-keypair"
-  public_key = file("${var.ssh_key_file}.pub")
-} 
--- part15-openstack-provider/5-get_images.tf
+++
@@ -1,7 +0,0 @@
-resource "openstack_images_image_v2" "debian" {
-  name             = "debian"
-  #image_source_url = "http://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img"
-  local_file_path = "debian-10.11.2-20211129-openstack-amd64.qcow2"
-  container_format = "bare"
-  disk_format      = "qcow2"
-}
