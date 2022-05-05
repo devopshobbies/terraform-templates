2efaffcad204daaf658b21635c614df34dad8c63
Mohammadreza <mrvashian@gmail.com>
2022-05-06 00:39:09 +0430

    add resources

---
+++ part04-gitlab-provider/resources.tf
@@ -0,0 +1,33 @@
+# Create project
+resource "gitlab_project" "project" {
+  name        = var.project.name
+  description = var.project.description
+  visibility_level = var.project.visibility_level
+  initialize_with_readme = true
+}
+
+# Create repository file
+resource "gitlab_repository_file" "fiel" {
+  project        = gitlab_project.project.id
+  file_path      = "test.txt"
+  branch         = "main"
+  content        = "lorem ipsum"
+  author_email   = "terraform@example.com"
+  author_name    = "Terraform"
+  commit_message = "feature: add test file"
+}
+
+# Manage license
+resource "gitlab_managed_license" "license" {
+  project         = gitlab_project.project.id
+  name            = "MIT license"
+  approval_status = "allowed"
+}
+
+# Create branch
+resource "gitlab_branch" "branch" {
+  name    = var.branch_name
+  ref     = "main"
+  project = gitlab_project.project.id
+}
+
