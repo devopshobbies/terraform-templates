a0e9539734566ca8df2724c748191374e57cb08d
Farshad <asadpoor.f@gmail.com>
2022-08-28 22:13:21 +0430

    update providers

--- part01-docker-provider/providers.tf
+++ part01-docker-provider/providers.tf
@@ -1,9 +1,17 @@
 # Adding terraform version and terraform provider version on Terraform block
+# Set the required provider and versions
 terraform {
   required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
   }
 }
+
+# Configure the docker provider
+# provider "docker" {
+#   host = "unix:///var/run/docker.sock"
+# }
