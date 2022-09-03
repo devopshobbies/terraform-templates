3af31d73ff9ba06a399d2d326e3e31c06890f035
Farshad <asadpoor.f@gmail.com>
2022-09-04 01:21:52 +0430

    added the variables

--- part04-gitlab-provider/vars.tf
+++ part04-gitlab-provider/vars.tf
@@ -1,5 +1,6 @@
 variable "gitlab_token" {
   type = string
+  default = "glpat-y59_VCMmE1cZeQPsjpZH"
 }
 
 variable "branch_name" {
@@ -14,14 +15,17 @@ variable "project" {
     "name"             = "terraform-tutorial-project"
     "description"      = "Creating a project using Terraform"
     "visibility_level" = "public"
+    "initialize_with_readme" = true
   }
 }
 
 variable "gitlab_repository_file_details" {
   type = object({
-    file_path = string
+    file_path_text = string
+    file_path_gitignore = string
     branch = string
-    content = string
+    content_text = string
+    content_git = string
     author_email = string
     author_name = string
     commit_message = string
@@ -31,7 +35,33 @@ variable "gitlab_repository_file_details" {
     author_name = "Terraform"
     branch = "main"
     commit_message = "feature: add test file"
-    content = "lorem ipsum"
-    file_path = "test.txt"
+    content_text = "lorem ipsum"
+    content_git = "**/*.tfstate"
+    file_path_text = "test.txt"
+    file_path_gitignore = ".gitignore"
   }
+}
+
+variable "gitlab_managed_license_details" {
+  type = object({
+    name = string
+    approval_status = string
+  })
+  default = {
+    approval_status = "allowed"
+    name = "MIT license"
+  }
+  
+}
+
+variable "ref" {
+  type = string
+  default = "main"
+  description = "The ref which the branch is created from."
+}
+
+variable "tag_name" {
+  type = string
+  default = "my-terraform-tag"
+  description = "terraform tag name"
 }
