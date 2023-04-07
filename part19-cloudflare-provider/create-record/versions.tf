e0a7c6b8c8aba9becb16de16196efb065da46541
mohammadne <mohammadne.dev@gmail.com>
2023-04-07 20:30:10 +0330

    feat: add provider versions

---
+++ part19-cloudflare-provider/create-record/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.2"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.token
+}
