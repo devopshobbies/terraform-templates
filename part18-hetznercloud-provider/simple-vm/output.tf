1781dae39113ace7f69a466b2a80a21e56496dac
Farshad <asadpoor.f@gmail.com>
2025-01-16 23:16:36 +0330

    Change output

--- part18-hetznercloud-provider/simple-vm/output.tf
+++ part18-hetznercloud-provider/simple-vm/output.tf
@@ -1,8 +1,19 @@
-
-output "server_status" {
-  value = hcloud_server.vpn.status
+output "server_ip" {
+  description = "The IP address of the created server"
+  value       = hcloud_server.node01.ipv4_address
 }
 
-output "server_ip" {
-  value = hcloud_server.vpn.ipv4_address
+output "server_id" {
+  description = "The ID of the created server"
+  value       = hcloud_server.node01.id
+}
+
+output "server_name" {
+  description = "The name of the created server"
+  value       = hcloud_server.node01.name
+}
+
+output "server_status" {
+  description = "The status of the created server"
+  value       = hcloud_server.node01.status
 }
