b600be3a9169be7f413eab7fddddd37727af4ebc
Farshad <asadpoor.f@gmail.com>
2025-01-17 12:26:07 +0330

    change output

--- part18-hetznercloud-provider/data_sources/main.tf
+++ part18-hetznercloud-provider/data_sources/main.tf
@@ -11,6 +11,38 @@ data "hcloud_locations" "all" {
 }
 
 output "locations" {
- value = data.hcloud_locations.all
+  value = data.hcloud_locations.all
 }
 
+resource "local_file" "locations_output" {
+  content = <<-EOT
+    Locations Data:
+    Descriptions:
+    %{ for desc in data.hcloud_locations.all.descriptions }
+    - ${desc}
+    %{ endfor }
+
+    Location IDs:
+    %{ for id in data.hcloud_locations.all.location_ids }
+    - ${id}
+    %{ endfor }
+
+    Locations Details:
+    %{ for loc in data.hcloud_locations.all.locations }
+    - Name: ${loc.name}
+      City: ${loc.city}
+      Country: ${loc.country}
+      Description: ${loc.description}
+      Latitude: ${loc.latitude}
+      Longitude: ${loc.longitude}
+      Network Zone: ${loc.network_zone}
+    %{ endfor }
+
+    Names:
+    %{ for name in data.hcloud_locations.all.names }
+    - ${name}
+    %{ endfor }
+  EOT
+
+  filename = "${path.module}/locations.txt"
+}
